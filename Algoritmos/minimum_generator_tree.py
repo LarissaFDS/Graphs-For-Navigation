73e0e0850953ebb94e363b65ae20fad2415059fa
Otavio Joshua <otaviomenezes574@gmail.com>
2025-10-31 19:32:21 -0300

    sketch of some functions to be implemented.

---
+++ Algoritmos/minimum_generator_tree.py
@@ -0,0 +1,8 @@
+""" 
+Implementation of Kruskal or Prim algorithm to find the Minimum Generator Tree of a graph.
+The goal is to get the Graph, represent it somehow and then apply one of the algorithms.
+
+"""
+
+def minimum_generator_tree(graph):
+    pass
